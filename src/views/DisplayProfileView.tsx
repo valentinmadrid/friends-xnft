e1e53b3f1933dcf5ab9e60c527d4e33e49c56687
Valentin Madrid <valentin@incility.com>
2022-10-26 18:13:11 +0200

    🔖 - Create Display Profile View

---
+++ src/views/DisplayProfileView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-xnft';
+
+const displayProfileView = () => {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+};
+
+export default displayProfileView;
